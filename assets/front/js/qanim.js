6ec10d1adab9acc6f6e7e8f7e426b924c8c0e20c
Piotr <piotr.rybarczyk1994@gmail.com>
2021-07-03 18:18:32 +0200

    animation script update

--- assets/front/js/qanim.js
+++ assets/front/js/qanim.js
@@ -1,11 +1,15 @@
 const animposition = []; //element position in document
 const animoffset = [];  //height of element
 const animcheck = [];   //checks if animation is done
-const animinterval = []; //animation timer for specific animation
 const animcounter = []; //animation progress value
 var element; // list of elements to be animated
-var rad = Math.PI / 210; // radian value with tweak
-var i = 0; // iteration variable... why you need more of them anyway...
+var rad = Math.PI / 180; // radian value for calculations
+var i = 0; // iteration value to get all elements data and check its position with scroll position
+var c = 0; // iteration value used in timer to pull conditions on all elements during tick
+var clock = 0; // one and only timer needed ;)
+var ms = 10; // timer interval value
+var min_speed = 0.4; //minimum speed of animation
+var mul_speed = 1.4; //animation speed multiplier
 
 window.onload = function () {
     //get all elements to be animated
@@ -13,42 +17,50 @@ window.onload = function () {
     //get all needed data 
     console.log(element.length);
     for (i; i < element.length; i++) {
-        animposition[i] = (element[i].getBoundingClientRect().top + window.scrollY)-600;
-        animoffset[i] = animposition[i] + (element[i].offsetHeight+300);
+        animposition[i] = (element[i].getBoundingClientRect().top + window.scrollY) - 600;
+        animoffset[i] = animposition[i] + (element[i].offsetHeight + 300);
         animcheck[i] = false;
         animcounter[i] = 0;
-        console.log(animposition[i]);
     }
     i = 0;
+    //everything runs on single interval so its less performance hungry :D
+    clock = setInterval(function () {
+        for (c; c < element.length; c++) {
+            if (animcheck[c] == true) {
+                if (animcounter[c] >= 100) {
+                    //making sure it ends where it should ends
+                    element[c].style.opacity = 100 + "%";
+                    if (element[c].classList.contains('qa_fade-right') == true) {
+                        element[c].style.right = 0 + "%";
+                    } else if (element[c].classList.contains('qa_fade-left') == true) {
+                        element[c].style.left = 0 + "%";
+                    } else if (element[c].classList.contains('qa_fade-down') == true) {
+                        element[c].style.top = 0 + "%";
+                    } else if (element[c].classList.contains('qa_fade-up') == true) {
+                        element[c].style.bottom = 0 + "%";
+                    }
+                } else {
+                    animcounter[c] = animcounter[c] + ((Math.cos(animcounter[c] * rad) * mul_speed) + min_speed); // oh yeah... that makes animation smooth baby ;p
+                    element[c].style.opacity = animcounter[c] + "%";
+                    if (element[c].classList.contains('qa_fade-right') == true) {
+                        element[c].style.right = (-100 + animcounter[c]) + "%";
+                    } else if (element[c].classList.contains('qa_fade-left') == true) {
+                        element[c].style.left = (-100 + animcounter[c]) + "%";
+                    } else if (element[c].classList.contains('qa_fade-down') == true) {
+                        element[c].style.top = (-100 + animcounter[c]) + "%";
+                    } else if (element[c].classList.contains('qa_fade-up') == true) {
+                        element[c].style.bottom = (-100 + animcounter[c]) + "%";
+                    }
+                }
+            }
+        }
+        c = 0;
+    }, ms);
 
 };
 window.onscroll = function () {
     for (i; i < element.length; i++) {
-        if (window.scrollY >= animposition[i] && window.scrollY <= animoffset[i] && animcheck[i] == false) {
-            animcheck[i] = true;
-            qanim(i); //this is where magic happens
-        }
+        if (window.scrollY >= animposition[i] && window.scrollY <= animoffset[i] && animcheck[i] == false) { animcheck[i] = true; } // checks if you scrolled to element position
     }
     i = 0;
-};
-
-function qanim(id) {
-    animinterval[id] = setInterval(function () {
-        if (animcounter[id] >= 100) {
-            clearInterval(animinterval[id]);
-        } else {
-            animcounter[id] = animcounter[id] + 2.5 * (Math.cos(animcounter[id] * rad));
-            element[id].style.opacity = animcounter[id] + "%";
-            if(element[id].classList.contains('qa_fade-right') == true){
-                element[id].style.right = (-100 + animcounter[id]) + "%";
-            }else if(element[id].classList.contains('qa_fade-left') == true){
-                element[id].style.left = (-100 + animcounter[id]) + "%";
-            }else if(element[id].classList.contains('qa_fade-down') == true){
-                element[id].style.top = (-100 + animcounter[id]) + "%";
-            }else if(element[id].classList.contains('qa_fade-up') == true){
-                element[id].style.bottom = (-100 + animcounter[id]) + "%";
-            }
-        }
-    }, 8)
-
 };
